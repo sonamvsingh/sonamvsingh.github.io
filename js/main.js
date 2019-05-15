e6371499caa4a1088ddb5c8b8ffcc015805e6753
sonamvsingh <sonamsingh4056@gmail.com>
2019-05-15 22:56:45 +0530

    img size reduction and slider fixes

--- js/main.js
+++ js/main.js
@@ -240,33 +240,16 @@
 	  //autoplay: true,
 	  cssEase: 'linear',
 	  responsive: [
-	    // {
-	    //   breakpoint: 1024,
-	    //   settings: {
-	    //     slidesToShow: 3,
-	    //     slidesToScroll: 3,
-	    //     infinite: true,
-	    //     dots: true
-	    //   }
-	    // },
-	    // {
-	    //   breakpoint: 600,
-	    //   settings: {
-	    //     slidesToShow: 2,
-	    //     slidesToScroll: 2
-	    //   }
-	    // },
 	    {
 	      breakpoint: 480,
 	      settings: {
 	        slidesToShow: 1,
 	        slidesToScroll: 1,
 	  			variableWidth: false,
-	  			arrows: false,
 	  			dots: false,
 	  			centerMode: false,
 	  			centerPadding: '0',
-
+	  			adaptiveHeight: true,
 	      }
 	    }
 	    // You can unslick at a given breakpoint now by adding:
