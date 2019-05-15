ec2b542ef212652d1fef929f04ed43588da8fa1b
sonamvsingh <sonamsingh4056@gmail.com>
2019-05-15 18:35:43 +0530

    Code cleanup

--- js/main.js
+++ js/main.js
@@ -238,7 +238,41 @@
 	  centerPadding: '60px',
 	  variableWidth: true,
 	  //autoplay: true,
-	  cssEase: 'linear'
+	  cssEase: 'linear',
+	  responsive: [
+	    // {
+	    //   breakpoint: 1024,
+	    //   settings: {
+	    //     slidesToShow: 3,
+	    //     slidesToScroll: 3,
+	    //     infinite: true,
+	    //     dots: true
+	    //   }
+	    // },
+	    // {
+	    //   breakpoint: 600,
+	    //   settings: {
+	    //     slidesToShow: 2,
+	    //     slidesToScroll: 2
+	    //   }
+	    // },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	  			variableWidth: false,
+	  			arrows: false,
+	  			dots: false,
+	  			centerMode: false,
+	  			centerPadding: '0',
+
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
 	});
 
 }());
